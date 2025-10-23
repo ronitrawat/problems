ad52f01629b101aeabab2d1444bc97330b872552
RONIT CR7 <100836959+ronitrawat@users.noreply.github.com>
2025-10-23 11:55:19 +0530

    Time: 111 ms (5.78%), Space: 92.4 MB (24.88%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.cpp
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -1,9 +1,11 @@
 class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
-        sort(nums.begin(),nums.end());
-        for(int i=0;i<nums.size()-1;i++){
-            if(nums[i]==nums[i+1]){
+     map<int,int> res;
+        for(int i:nums){
+
+            res[i]++;
+            if(res[i]>1){
                 return true;
             }
         }
