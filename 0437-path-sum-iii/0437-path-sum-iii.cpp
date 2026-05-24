0c4bcf7de6857ad064278c20cda4cb6de4d44711
RONIT CR7 <100836959+ronitrawat@users.noreply.github.com>
2026-05-24 19:07:32 +0530

    Time: 0 ms (100%), Space: 20.8 MB (31.99%) - LeetHub

--- 0437-path-sum-iii/0437-path-sum-iii.cpp
+++ 0437-path-sum-iii/0437-path-sum-iii.cpp
@@ -12,27 +12,37 @@
 class Solution {
 public:
     int path=0;
-    void solve(TreeNode* root,long long int targetSum){
-        if(!root){
-            return;
+    void solve(TreeNode* root,int targetSum, long long int currSum,unordered_map<long long int,int>& m){
+       
+        if(root==NULL){
+         return;
         }
-        if(targetSum==root->val){
+        currSum+=root->val;
+        if(currSum==targetSum){
             path++;
-            
         }
         
+        if(m.find(currSum-targetSum)!=m.end()){
+            path+=m[currSum-targetSum];
+
+        }
+        m[currSum]++;
+
         
-        solve(root->left,targetSum-root->val);
-        solve(root->right,targetSum-root->val);
-        
+        solve(root->left,targetSum,currSum,m);
+        solve(root->right,targetSum,currSum,m);
+        m[currSum]--;
 
     }
+    
+
     int pathSum(TreeNode* root, int targetSum) {
+       int currSum=0;
+       unordered_map<long long int,int> m;
        if(root){
        
-        solve(root,targetSum);
-        pathSum(root->left,targetSum);
-        pathSum(root->right,targetSum);
+        solve(root,targetSum,currSum,m);
+        
         }
         return path;
     }
