aff22e56c31a6db8a3462cff6783f4af36dab263
RONIT CR7 <100836959+ronitrawat@users.noreply.github.com>
2026-06-01 17:01:34 +0530

    Time: 7 ms (51.99%), Space: 88.4 MB (39.7%) - LeetHub

--- 1305-all-elements-in-two-binary-search-trees/1305-all-elements-in-two-binary-search-trees.cpp
+++ 1305-all-elements-in-two-binary-search-trees/1305-all-elements-in-two-binary-search-trees.cpp
@@ -6,28 +6,61 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right)
+ *         : val(x), left(left), right(right) {}
  * };
  */
+
 class Solution {
 public:
-    vector<int> a;
-    
-    void inorder( TreeNode* root){
-        if(root==NULL){
+
+    void inorder(TreeNode* root, vector<int>& v) {
+
+        if(root == NULL) {
             return;
         }
-  
-        
-        inorder(root->left);
-        a.push_back(root->val);
-        inorder(root->right);
+
+        inorder(root->left, v);
+
+        v.push_back(root->val);
+
+        inorder(root->right, v);
     }
 
     vector<int> getAllElements(TreeNode* root1, TreeNode* root2) {
-     inorder(root1);
-     inorder(root2);
-     sort(a.begin(),a.end());
-     return a;
+
+        vector<int> a, b;
+
+        inorder(root1, a);
+        inorder(root2, b);
+
+        vector<int> ans;
+
+        int i = 0;
+        int j = 0;
+
+        while(i < a.size() && j < b.size()) {
+
+            if(a[i] <= b[j]) {
+                ans.push_back(a[i]);
+                i++;
+            }
+            else {
+                ans.push_back(b[j]);
+                j++;
+            }
+        }
+
+        while(i < a.size()) {
+            ans.push_back(a[i]);
+            i++;
+        }
+
+        while(j < b.size()) {
+            ans.push_back(b[j]);
+            j++;
+        }
+
+        return ans;
     }
 };
