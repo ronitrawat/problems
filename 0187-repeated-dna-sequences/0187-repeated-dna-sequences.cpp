5984f41cc3f12aa9540da00cdbc920c96fd29420
RONIT CR7 <100836959+ronitrawat@users.noreply.github.com>
2026-06-17 17:37:22 +0530

    Time: 60 ms (16.04%), Space: 32.7 MB (5.8%) - LeetHub

--- 0187-repeated-dna-sequences/0187-repeated-dna-sequences.cpp
+++ 0187-repeated-dna-sequences/0187-repeated-dna-sequences.cpp
@@ -1,21 +1,53 @@
+class Trie{
+public:
+
+    Trie* child[4]={};
+
+    int cnt=0;
+};
+
 class Solution {
 public:
+
+    int getIdx(char c){
+
+        if(c=='A') return 0;
+        if(c=='C') return 1;
+        if(c=='G') return 2;
+
+        return 3;
+    }
+
     vector<string> findRepeatedDnaSequences(string s) {
 
-        unordered_set<string> seen;
-        unordered_set<string> repeated;
+        Trie* root = new Trie();
 
-        for(int i=0;i+9<s.size();i++){
+        vector<string> ans;
+
+        int n = s.size();
+
+        for(int i=0;i+9<n;i++){
+
+            Trie* node = root;
 
             string cur = s.substr(i,10);
 
-            if(seen.count(cur))
-                repeated.insert(cur);
-            else
-                seen.insert(cur);
+            for(char c : cur){
+
+                int idx = getIdx(c);
+
+                if(node->child[idx]==NULL)
+                    node->child[idx]=new Trie();
+
+                node=node->child[idx];
+            }
+
+            node->cnt++;
+
+            if(node->cnt==2)
+                ans.push_back(cur);
         }
 
-        return vector<string>(repeated.begin(),
-                              repeated.end());
+        return ans;
     }
 };
