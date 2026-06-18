4056535002b3543d37fdbd9939910da6b14c4325
RONIT CR7 <100836959+ronitrawat@users.noreply.github.com>
2026-06-18 06:54:59 +0530

    Time: 0 ms (100%), Space: 12.4 MB (8.1%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.cpp
+++ 0014-longest-common-prefix/0014-longest-common-prefix.cpp
@@ -1,22 +1,78 @@
+class Trie{
+public:
+
+    Trie* child[26] = {};
+
+    bool isEnd = false;
+};
+
 class Solution {
 public:
-    string longestCommonPrefix(vector<string>& strs) {
-       string res="";
-       string  s=strs[0];
-       for (int i=0;i<s.length();i++){
-        char c=s[i];
-        bool put=true;
-        for(int j=0;j<strs.size();j++)
-       { string a=strs[j];
-        if(a[i]!=c){
-            put=false;
-            break;
+
+    Trie* root = new Trie();
+
+    void insert(string word){
+
+        Trie* node = root;
+
+        for(char c : word){
+
+            int idx = c - 'a';
+
+            if(node->child[idx] == NULL)
+                node->child[idx] = new Trie();
+
+            node = node->child[idx];
         }
 
+        node->isEnd = true;
+    }
 
-       }if(put){
-       res.push_back(c);}
-       else{break;}
-    }return res;
+    int countChild(Trie* node,
+                   int &idx){
+
+        int cnt = 0;
+
+        for(int i=0;i<26;i++){
+
+            if(node->child[i]){
+
+                cnt++;
+                idx = i;
+            }
+        }
+
+        return cnt;
+    }
+
+    string longestCommonPrefix(
+            vector<string>& strs) {
+
+        for(auto &s : strs)
+            insert(s);
+
+        string ans = "";
+
+        Trie* node = root;
+
+        while(node){
+
+            int idx = -1;
+
+            int cnt =
+            countChild(node, idx);
+
+            if(cnt != 1)
+                break;
+
+            if(node->isEnd)
+                break;
+
+            ans += char('a' + idx);
+
+            node = node->child[idx];
+        }
+
+        return ans;
     }
 };
