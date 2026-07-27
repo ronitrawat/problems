6051855a1617ed6053a8b07ec21d38cd5a5633b7
RONIT CR7 <100836959+ronitrawat@users.noreply.github.com>
2026-07-27 08:31:05 +0530

    Time: 0 ms (100%), Space: 10.6 MB (86.28%) - LeetHub

--- 1049-last-stone-weight-ii/1049-last-stone-weight-ii.cpp
+++ 1049-last-stone-weight-ii/1049-last-stone-weight-ii.cpp
@@ -5,19 +5,17 @@ public:
         for(int i=0;i<stones.size();i++){
            sum+=stones[i];        }
         int s1=sum/2;
-vector<vector<bool>> dp(stones.size()+1,vector<bool>(s1+1));
-        for(int j=0;j<=s1;j++){
-            dp[0][j]=false;}for(int i=0;i<=stones.size();i++){
-            dp[i][0]=true;}for(int i=1;i<=stones.size();i++){
-            for(int j=1;j<=s1;j++){
-                if(stones[i-1]<=j){
-                    dp[i][j]=dp[i-1][j-stones[i-1]] || dp[i-1][j];}
-                else{
-                    dp[i][j]=dp[i-1][j];
-                }     }}
+vector<bool> dp(s1+1);
+dp[0]=true;
+        for(int j=1;j<=s1;j++){
+            dp[j]=false;}
+           for(int i=1;i<=stones.size();i++){
+            for(int j=s1;j>=stones[i-1];j--){
+                dp[j]=dp[j] || dp[j-stones[i-1]];
+            }}
         int best=0;
         for(int j=s1;j>=0;j--){
-            if(dp[stones.size()][j]){
+            if(dp[j]){
                 best=j;
                 break;
             }
