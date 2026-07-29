d9640e933f3dae77a00bd8f1c4c624cc813af12e
RONIT CR7 <100836959+ronitrawat@users.noreply.github.com>
2026-07-29 08:06:25 +0530

    Time: 27 ms (95.21%), Space: 11.8 MB (98.59%) - LeetHub

--- 0879-profitable-schemes/0879-profitable-schemes.cpp
+++ 0879-profitable-schemes/0879-profitable-schemes.cpp
@@ -1,29 +1,40 @@
 class Solution {
 public:
-    int profitableSchemes(int n, int minProfit, vector<int>& group, vector<int>& profit) {
-        vector<vector<vector<int>>> dp(group.size()+1,vector<vector<int>>(n+1,vector<int>(minProfit+1,0)));
+    int profitableSchemes(int n, int minProfit,
+                          vector<int>& group,
+                          vector<int>& profit) {
 
-        dp[0][0][0]=1;
-        int MOD=1e9+7;
+        const int MOD = 1e9 + 7;
 
-        for(int i=1;i<=group.size();i++){
+        vector<vector<int>> dp(n + 1, vector<int>(minProfit + 1, 0));
 
-            int members=group[i-1];
-            int pro=profit[i-1];
+        dp[0][0] = 1;
 
-            for(int j=0;j<=n;j++){
-                for(int k=0;k<=minProfit;k++){
-                    dp[i][j][k]=dp[i-1][j][k];
+        for (int i = 0; i < group.size(); i++) {
 
-                    if(j>=members){
-                        dp[i][j][k]=(dp[i][j][k]+dp[i-1][j-members][max(0,k-pro)])% MOD;
-                    }
+            int members = group[i];
+            int earn = profit[i];
+
+            // Backward because each crime can be chosen only once
+            for (int j = n; j >= members; j--) {
+
+                for (int k = minProfit; k >= 0; k--) {
+
+                    int newProfit = min(minProfit, k + earn);
+
+                    dp[j][newProfit] =
+                        (dp[j][newProfit] +
+                         dp[j - members][k]) % MOD;
                 }
             }
         }
-        int ans=0;
-        for(int j=0;j<=n;j++){
-           ans=(ans+dp[group.size()][j][minProfit])%MOD;
-        }    
-        return ans;}
+
+        int ans = 0;
+
+        for (int j = 0; j <= n; j++) {
+            ans = (ans + dp[j][minProfit]) % MOD;
+        }
+
+        return ans;
+    }
 };
