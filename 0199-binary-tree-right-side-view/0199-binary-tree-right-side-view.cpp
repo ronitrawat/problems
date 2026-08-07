be86f3297bfe25116e12ce572db91a21b9edf648
RONIT CR7 <100836959+ronitrawat@users.noreply.github.com>
2026-08-07 07:24:08 +0530

    Time: 0 ms (100%), Space: 15.1 MB (22.11%) - LeetHub

--- 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.cpp
+++ 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.cpp
@@ -9,43 +9,30 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
-
 class Solution {
 public:
-
-     vector<vector<int>> ans;
-    vector<int> a;
-    void level(TreeNode *root){
-        queue<TreeNode *> q;
-        if(root!=NULL){
-          q.push(root);  
-        }
-        while(!q.empty()){
-        
-            int size=q.size();
-            while(size--){
-            TreeNode* n=q.front();
-            a.push_back(n->val);
-            q.pop();
-            if(n->right!=NULL){
-            q.push(n->right);
-            }
-            if(n->left!=NULL){
-            q.push(n->left);}
-            }
-            ans.push_back(a);
-            a.clear();
-        }
-    }
-
-
     vector<int> rightSideView(TreeNode* root) {
-        level(root);
-        vector<int> b;
-        int n=ans.size();
-        for(int i=0;i<n;i++){
-            b.push_back(ans[i][0]);
+     queue<TreeNode*> q;
+     vector<int> ans;
+     vector<int> temp;
+     if(root==NULL){
+      return ans;
+     }  
+     q.push(root);
+     while(!q.empty()){
+        int s =q.size();
+        while(s--){
+            TreeNode* it=q.front();
+            q.pop();
+            temp.push_back(it->val);
+            if(it->right){
+            q.push(it->right);}
+            if(it->left){
+            q.push(it->left);}
         }
-        return b;
+        ans.push_back(temp[0]);
+        temp.clear();
+     }
+     return ans;
     }
 };
