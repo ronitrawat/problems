87166bcf338fc384680b9ab6d41be59142703641
RONIT CR7 <100836959+ronitrawat@users.noreply.github.com>
2026-08-11 07:31:49 +0530

    Time: 0 ms (100%), Space: 28.6 MB (5.42%) - LeetHub

--- 0106-construct-binary-tree-from-inorder-and-postorder-traversal/0106-construct-binary-tree-from-inorder-and-postorder-traversal.cpp
+++ 0106-construct-binary-tree-from-inorder-and-postorder-traversal/0106-construct-binary-tree-from-inorder-and-postorder-traversal.cpp
@@ -11,45 +11,31 @@
  */
 class Solution {
 public:
-    unordered_map<int,int> m;
-    void mapping(vector<int> inorder){
+ map<int,int> m;
+
+    TreeNode* build(vector<int> &postorder,int &pStart,int iStart,int iEnd){
+        if(pStart<0){
+            return NULL;
+        }
+        if(iStart>iEnd){
+            return NULL;
+        }
+        int element =postorder[pStart--];
+        TreeNode* root=new TreeNode(element);
+
+        int idx=m[element];
+        root->right=build(postorder,pStart,idx+1,iEnd);
+        root->left=build(postorder,pStart,iStart,idx-1);
+        
+
+        return root;
+    }
+    TreeNode* buildTree(vector<int>& inorder, vector<int>& postorder) {
         for(int i=0;i<inorder.size();i++){
             m[inorder[i]]=i;
         }
-    }
-
-    
-    TreeNode* solve(vector<int>& inorder,vector<int>& postorder,int& postorderIndex,int inorderStart,int inorderEnd ){
-      
-    if(postorderIndex<0){
-        return NULL;
-    }  
-    if(inorderStart>inorderEnd){
-        return NULL;
-    }
-    int ele=postorder[postorderIndex--];
-    TreeNode* root=new TreeNode(ele);
-
-
-    int inorderIndex=m[ele];
-
-    root->right=solve(inorder,postorder,postorderIndex,inorderIndex+1,inorderEnd);
-    root->left=solve(inorder,postorder,postorderIndex,inorderStart,inorderIndex-1);
-    
-
-    return root;
-    }
-
-
-
-    TreeNode* buildTree(vector<int>& inorder, vector<int>& postorder) {
-       int n=postorder.size();
-        int postorderIndex=n-1;
-        int inorderStart=0;
-        int inorderEnd=inorder.size()-1;
-        mapping(inorder);
-
-        TreeNode* root=solve(inorder,postorder,postorderIndex,inorderStart,inorderEnd);
+        int pStart=postorder.size()-1;
+        TreeNode* root=build(postorder,pStart,0,inorder.size()-1);
         return root;
     }
 };
