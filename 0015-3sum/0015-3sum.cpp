273a961827522b602883d8b4e9eddb6c89dc0086
RONIT CR7 <100836959+ronitrawat@users.noreply.github.com>
2026-08-30 09:59:16 +0530

    Time: 51 ms (41.69%), Space: 29 MB (92.42%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -2,39 +2,51 @@ class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
         vector<vector<int>> ans;
+
         sort(nums.begin(), nums.end());
 
-        for(int i = 0; i < nums.size(); i++) {
+        for (int i = 0; i < nums.size() - 2; i++) {
 
-            if(i > 0 && nums[i] == nums[i - 1]) continue;  // skip duplicate i
+           
+            if (i > 0 && nums[i] == nums[i - 1]) {
+                continue;
+            }
 
-            int target = -nums[i];
             int low = i + 1;
             int high = nums.size() - 1;
 
-            while(low < high) {
-                int sum = nums[low] + nums[high];
+            while (low < high) {
 
-                if(sum == target) {
-                    ans.push_back({nums[i], nums[low], nums[high]});
+                int sum = nums[i] + nums[low] + nums[high];
+
+                if (sum < 0) {
                     low++;
-                    high--;
-
-                    // skip duplicates for low
-                    while(low < high && nums[low] == nums[low - 1]) low++;
-
-                    // skip duplicates for high
-                    while(low < high && nums[high] == nums[high + 1]) high--;
-
                 }
-                else if(sum > target) {
+                else if (sum > 0) {
                     high--;
                 }
                 else {
+               
+                    ans.push_back({
+                        nums[i],
+                        nums[low],
+                        nums[high]
+                    });
+
                     low++;
+                    high--;
+
+                    while (low < high && nums[low] == nums[low - 1]) {
+                        low++;
+                    }
+
+                    while (low < high && nums[high] == nums[high + 1]) {
+                        high--;
+                    }
                 }
             }
         }
+
         return ans;
     }
 };
