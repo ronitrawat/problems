0e033288b7a9fee1f681b80729f98c8cc164b44d
RONIT CR7 <100836959+ronitrawat@users.noreply.github.com>
2026-09-16 17:56:33 +0530

    Time: 0 ms (100%), Space: 18.6 MB (8.38%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.cpp
+++ 0733-flood-fill/0733-flood-fill.cpp
@@ -4,17 +4,25 @@ public:
     int c;
     int dx[4]={1,0,-1,0};
     int dy[4]={0,1,0,-1};
-    void dfs(int i,int j, int org,int color,vector<vector<int>> &image){
-        if(i<0 || j<0 || i>=r || j>=c || image[i][j]!=org){
-            return ;
-        }
+    void bfs(int i,int j, int org,int color,vector<vector<int>> &image){
+        queue<pair<int,int>> q;
+        q.push({i,j});
         image[i][j]=color;
 
-        for(int k=0;k<4;k++){
-            int ii=i+dx[k];
-            int jj=j+dy[k];
-
-            dfs(ii,jj,org,color,image);
+        while(!q.empty()){
+            auto f=q.front();
+            q.pop();
+            int m=f.first;
+            int n=f.second;
+            for(int k=0;k<4;k++){
+                int ii=m+dx[k];
+                int jj=n+dy[k];
+                if(ii<0 || jj<0 || ii>=r || jj >=c || image[ii][jj]!=org){
+                    continue;
+                }
+                q.push({ii,jj});
+                image[ii][jj]=color;
+            }
         }
     }
     vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
@@ -27,7 +35,7 @@ public:
             return image;
         }
 
-        dfs(sr,sc,org,color,image);
+        bfs(sr,sc,org,color,image);
 
         return image;
     }
